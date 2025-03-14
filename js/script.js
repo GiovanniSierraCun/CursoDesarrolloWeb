872bf593423988e52915e031c900e4f4dac6b5f2
GiovanniSierraCun <fredy_sierra@cun.edu.co>
2025-03-13 21:18:51 -0500

    etiquetas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 
-
-
 function originaltexto(){
     document.getElementById('demo').innerHTML='este  texto  se  va   a  cambiar  con  JS';
    }
@@ -8,3 +6,5 @@ function originaltexto(){
    function cambiartexto(){
     document.getElementById('demo').innerHTML='Este  texto  cambia';
    }
+
+originalTexto1=()=> document.getElementById('demo1').innerHTML='teto que  se  va  cambiar con  JS'
