459fe3fe6425d47e8f5fb35ca1e58209eba0aaea
GiovanniSierraCun <fredy_sierra@cun.edu.co>
2025-03-13 21:04:29 -0500

    funciones y  archivos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+
+function originaltexto(){
+    document.getElementById('demo').innerHTML='este  texto  se  va   a  cambiar  con  JS';
+   }
+
+   function cambiartexto(){
+    document.getElementById('demo').innerHTML='Este  texto  cambia';
+   }
